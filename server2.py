101b94c3c71b532b77c82a6962c36ccc7190e3b4
sanchitabasak <sanchita.basak@vanderbilt.edu>
2017-08-31 00:59:33 -0500

    Add existing file

--- server2.py
+++ server2.py
@@ -9,6 +9,6 @@ sock.bind("tcp://127.0.0.1:5678")
 
 
 while True:
-message = sock.recv()
-sock.send(message + "\n" + message)
-print message
+           message = sock.recv()
+           sock.send(message + "\n" + message)
+           print message
